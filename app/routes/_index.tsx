3958b0f7173ef720b718af658c6cbc07ef5a8f04
Ubuntu <ubuntu@ip-10-0-7-211.ap-northeast-1.compute.internal>
2024-07-04 00:22:38 +0000

    fix: change button to link

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,6 @@
 import type { MetaFunction } from '@remix-run/node';
-import { useRouteLoaderData } from '@remix-run/react';
-import { Button } from '~/components/ui/button';
+import { Link, useRouteLoaderData } from '@remix-run/react';
+import { buttonVariants } from '~/components/ui/button';
 import type { loader } from '~/root';
 
 export const meta: MetaFunction = () => {
@@ -15,14 +15,31 @@ export default function Index() {
   return (
     <div>
       <header className="flex items-center justify-between px-5 py-3">
-        <p className="text-4xl font-bold select-none">Todos</p>
-        <div>
+        <Link to="/" className="text-4xl font-bold select-none">
+          Todos
+        </Link>
+        <div className="space-x-3">
           {data?.isLoggedIn ? (
-            <Button>Sign out</Button>
+            <Link
+              to="/auth/sign-out"
+              className={buttonVariants({ variant: 'outline' })}
+            >
+              Sign out
+            </Link>
           ) : (
             <>
-              <Button>Sign in</Button>
-              <Button>Sign up</Button>
+              <Link
+                to="/auth/sign-in"
+                className={buttonVariants({ variant: 'outline' })}
+              >
+                Sign in
+              </Link>
+              <Link
+                to="/auth/sign-up"
+                className={buttonVariants({ variant: 'outline' })}
+              >
+                Sign up
+              </Link>
             </>
           )}
         </div>
